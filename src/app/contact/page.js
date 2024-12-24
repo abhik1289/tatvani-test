abf0fd37397f245d845ffa7092d57396bcabc964
Soumyadip Maity <22053029@kiit.ac.in>
2024-12-25 01:27:09 +0530

    Add Contact page with ContactForm component and background image

---
+++ src/app/contact/page.js
@@ -0,0 +1,10 @@
+import ContactForm from "@/components/ContactPage/ContactForm"
+
+export default function Contact() {
+  return (
+    <div>
+      <img src=" https://t3.ftcdn.net/jpg/05/30/96/04/360_F_530960431_c8fPd3HansYvrSJ4fJxZqp9OhjQmYoll.jpg" alt="" className="w-full  object-cover object-center"/>
+      <ContactForm/>
+    </div>
+  )
+}
