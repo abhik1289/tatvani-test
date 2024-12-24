289ea35288e8db0c74f163eb2dc2116eaa563c4f
Soumyadip Maity <22053029@kiit.ac.in>
2024-12-25 01:28:27 +0530

    Add Submission page with SubmissionForm component and background image

---
+++ src/app/submission/page.js
@@ -0,0 +1,12 @@
+import SubmissionForm from "@/components/SubmissionPage/SubmissionForm"
+
+export default function Submission() {
+  return (
+    <div className="dark:bg-gray-800">
+
+<img src="https://images.pexels.com/photos/1308881/pexels-photo-1308881.jpeg?cs=srgb&dl=pexels-soldiervip-1308881.jpg&fm=jpg" alt="" className="w-full h-96 object-cover object-center"/>
+
+      <SubmissionForm/>
+    </div>
+  )
+}
