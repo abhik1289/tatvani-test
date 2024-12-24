b1300dfcc724bccafd3f8ede2ed34061d44007f0
Soumyadip Maity <22053029@kiit.ac.in>
2024-12-25 01:28:10 +0530

    Add ReviewPostPage component with Sidebar and Header

---
+++ src/app/dashboard/review/page.js
@@ -0,0 +1,17 @@
+import ReviewPost from '../../components/AdminPage/reviewPost';
+import Sidebar from '../../components/Essentials/Sidebar';
+import Header from '../../components/Essentials/Header';
+
+const ReviewPostPage = () => {
+  return (
+    <div className="admin-dashboard flex">
+      <Sidebar />
+      <div className="flex-1 p-8 bg-gray-800 pt-16  pl-64">
+        <Header />
+        <ReviewPost/>
+      </div>
+    </div>
+  );
+};
+
+export default ReviewPostPage;
