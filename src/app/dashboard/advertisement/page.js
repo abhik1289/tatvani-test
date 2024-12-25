af5027e80f4d607e889c45d4f8813b128195fd41
Soumyadip Maity <22053029@kiit.ac.in>
2024-12-26 01:51:50 +0530

    Enforce dynamic rendering in AddAdvertisePage to prevent static generation issues and improve formatting

--- src/app/dashboard/advertisement/page.js
+++ src/app/dashboard/advertisement/page.js
@@ -2,11 +2,13 @@ import Sidebar from '@/components/Essentials/Sidebar';
 import Header from '@/components/Essentials/Header';
 import AddAdvertise from '@/components/AdminPage/addAdvertise';
 
+export const dynamic = 'force-dynamic'; // Prevent static generation issues
+
 const AddAdvertisePage = () => {
   return (
     <div className="admin-dashboard flex">
       <Sidebar />
-      <div className="flex-1 p-8 bg-gray-800 pt-16  pl-64">
+      <div className="flex-1 p-8 bg-gray-800 pt-16 pl-64">
         <Header />
         <AddAdvertise />
       </div>
