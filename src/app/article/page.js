286f7828d759c2f7a7638250174dfdd047a83923
Soumyadip Maity <22053029@kiit.ac.in>
2024-12-26 01:52:10 +0530

    Improve formatting in Article component by adding spaces for consistency

--- src/app/article/page.js
+++ src/app/article/page.js
@@ -1,12 +1,13 @@
 import AdvertisementSection from "@/components/HomePage/AdvertisementSection";
 import ExclusiveNews from "@/components/NewsPage/ExclusiveNews";
 import NewsSlider from "@/components/NewsPage/NewsSlider";
+
 export default function Article() {
   return (
     <div>
       <NewsSlider />
-      <ExclusiveNews/>
-      <AdvertisementSection/>
+      <ExclusiveNews />
+      <AdvertisementSection />
     </div>
   );
 }
